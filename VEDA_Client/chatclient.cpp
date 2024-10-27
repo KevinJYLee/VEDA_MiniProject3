abdd4784311a6f9509589d2b03fe399187cb7d59
suping4 <kimjs158@hanyang.ac.kr>
2024-10-27 22:29:41 +0900

    .

--- VEDA_Client/chatclient.cpp
+++ VEDA_Client/chatclient.cpp
@@ -128,28 +128,6 @@ void ChatClient::processFileMessage(const QJsonObject& jsonObj)
     }
 }
 
-void ChatClient::processFileMessage(const QJsonObject& jsonObj)
-{
-    QString fileName = jsonObj["name"].toString();
-    QByteArray fileData = QByteArray::fromBase64(jsonObj["data"].toString().toLatin1());
-
-    // 파일 저장 대화상자 표시
-    QString savePath = QFileDialog::getSaveFileName(nullptr, "Save File", fileName, "All Files (*)");
-    if (!savePath.isEmpty()) {
-        QFile file(savePath);
-        if (file.open(QIODevice::WriteOnly)) {
-            file.write(fileData);
-            file.close();
-            emit fileReceived(fileName);
-            qDebug() << "File received and saved:" << fileName;
-        } else {
-            qDebug() << "Failed to save received file:" << fileName;
-        }
-    } else {
-        qDebug() << "File save cancelled by user:" << fileName;
-    }
-}
-
 void ChatClient::processLoginResponse(const QJsonObject& response)
 {
     bool isSuccess = (response["status"].toString() == "success");
